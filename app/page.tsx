1e82d4cb9b1d22b941a4ec9eae6b262a3ec7fd32
Obaloluwa Olusoji <olusojiobah@gmail.com>
2023-04-18 07:45:31 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,65 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import AssetRow from "@/lib/assetRow";
+import { useEffect, useState } from "react";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import { Typography } from "@mui/material";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [amount, setAmount] = useState(1);
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [primaryCurrency, setPrimaryCurrency] = useState("USD");
+  const [secondaryCurrency, setSecondaryCurrency] = useState("AED");
+  const [amountFromPrimary, setAmountFromPrimary] = useState(true);
+  const [rate, setRate] = useState<number | null>();
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  const API_KEY: string | undefined = process.env.REACT_APP_FX_API;
+  const CURRENCY_URL: string = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
+  const EXCHANGE_URL: string = `https://v6.exchangerate-api.com/v6/${API_KEY}/pair/${primaryCurrency}/${secondaryCurrency}`;
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+  const formatNum = (num: number): number => {
+    return +num.toFixed(2);
+  };
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  let primaryAmount: number, secondaryAmount: number;
+  if (amountFromPrimary) {
+    primaryAmount = formatNum(+amount);
+    secondaryAmount = formatNum(+amount * rate);
+  } else {
+    secondaryAmount = formatNum(+amount);
+    primaryAmount = formatNum(+amount / rate);
+  }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
+  useEffect(() => {
+    fetch(CURRENCY_URL)
+      .then((res) => res.json())
+      .then((resJson) => {
+        const currency = Object.keys(resJson.conversion_rates)[1];
+        setCurrencyOptions([...Object.keys(resJson.conversion_rates)]);
+        setPrimaryCurrency(resJson.base_code);
+        setSecondaryCurrency(currency);
+        setRate(resJson.conversion_rates[currency]);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  useEffect(() => {
+    if (primaryCurrency != null && secondaryCurrency != null) {
+      fetch(EXCHANGE_URL)
+        .then((res) => res.json())
+        .then((resJson) => setRate(resJson.conversion_rate));
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [primaryCurrency, secondaryCurrency]);
+
+  const handlePrimaryAmountChange = (e) => {
+    setAmount(e.target.value);
+    setAmountFromPrimary(true);
+  };
+
+  const handleSecondaryAmountChange = (e) => {
+    setAmount(e.target.value);
+    setAmountFromPrimary(false);
+  };
+
+  return <main></main>;
 }
